941a9f704d285f0961784406dd25b4adf86dade1
Souparno <souparna2000shee@gmail.com>
2025-01-04 16:37:25 +0530

    ch

--- src/main.jsx
+++ src/main.jsx
@@ -2,19 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.scss'
-import { BrowserRouter } from "react-router-dom"
+import { HashRouter } from "react-router-dom"
 import { ContextProvider } from './context/userContext.jsx'
 
 
 export const server = 'http://localhost:8000/jdm/api/v1'
 createRoot(document.getElementById('root')).render(
   <ContextProvider>
-    <BrowserRouter>
+    <HashRouter>
 
       <StrictMode>
         <App />
       </StrictMode>
 
-    </BrowserRouter>
+    </HashRouter>
   </ContextProvider>
 )
